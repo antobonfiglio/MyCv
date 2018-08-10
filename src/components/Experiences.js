6465aba1a88628de49f54351e58b61dd5ff31568
Antonino Bonfiglio <Antonino.Bonfiglio@health.telstra.com>
2018-08-10 10:46:36 +1000

    Added more info in cv.json

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -35,12 +35,12 @@ const Experiences = ({experiences}) => {
     };
 
     return(    
-    <React.Fragment>
-             <div className="w3-container w3-card w3-white w3-margin-bottom">             
-             <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
-                {experiences.map( (experience, i) => { return Experience(experience, i)  } )}   
-             </div>             
-    </React.Fragment>
+        <React.Fragment>
+                <div className="w3-container w3-card w3-white w3-margin-bottom">             
+                <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+                    {experiences.map( (experience, i) => { return Experience(experience, i)  } )}   
+                </div>             
+        </React.Fragment>
     );
 }
 
