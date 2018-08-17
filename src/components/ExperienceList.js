80c71c453d038fdb6308fa61380da66e6793280b
Antonino Bonfiglio <antobonfiglio@hotmail.com>
2018-08-18 01:43:12 +1000

    Css fixes

--- src/components/ExperienceList.js
+++ src/components/ExperienceList.js
@@ -89,12 +89,18 @@ export default class  ExperienceList extends Component {
                      <ul>
                         {
                         experience.Activities.map((activity,i) =>                
-                            <li key={i}>
-                                <input type="text" 
-                                       style={{width:95+'%',margin:'0px 10px 10px 0px'}}                               
-                                       value={activity}                                
-                                       onChange={this.activityInputChange(index, i)} />
-                               <button type="button" onClick={this.removeActivity(index, i)} className="small">-</button>
+                            <li key={i} style={{position:'relative'}}>
+                                <div style={{marginRight:40, marginBottom:10}}>
+                                    <input type="text" 
+                                        style={{width:100+'%'}}
+                                        className="activity"
+                                        value={activity}                                
+                                        onChange={this.activityInputChange(index, i)} />
+                                 </div>                                          
+                                    <button  
+                                    style={{width:30,position:'absolute',right:0,top:0}}
+                                        type="button" 
+                                        onClick={this.removeActivity(index, i)}>-</button>                              
                             </li>
                             )
                         }
